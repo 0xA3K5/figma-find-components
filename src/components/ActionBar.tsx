ec3e7d9196ef398a79390853ffb4df2c003a0a6d
ak <a.kemalakcay@gmail.com>
2023-11-28 02:39:17 +0300

    add `IconButton` component

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,7 +1,8 @@
-import { IconButton, IconLinkBreak32 } from '@create-figma-plugin/ui';
+import { IconLinkBreak32 } from '@create-figma-plugin/ui';
 import { h, JSX } from 'preact';
 import { emit } from '@create-figma-plugin/utilities';
 import { IComponentInstance, DetachInstances, DeleteInstances } from '../types';
+import { IconButton } from './button';
 
 function TrashIcon() {
   return (
@@ -48,17 +49,16 @@ export default function ActionBar({ checkedInstances, grouped }: Props): JSX.Ele
 
   return (
     <div
-      className="group fixed bottom-0 z-20 flex items-center gap-4 px-2 py-4"
+      className="group fixed bottom-4 z-20 flex w-full items-center justify-center"
     >
-      <IconButton onClick={handleDelete}>
-        <TrashIcon />
-      </IconButton>
-      <IconButton onClick={handleDetach}>
-        <IconLinkBreak32 />
-      </IconButton>
-      <IconButton>
-        <TrashIcon />
-      </IconButton>
+      <div className="flex w-fit gap-1 rounded-xl border border-black border-opacity-10 bg-black bg-opacity-5 px-3 py-2 dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-5">
+        <IconButton onClick={handleDelete}>
+          <TrashIcon />
+        </IconButton>
+        <IconButton onClick={handleDetach}>
+          <IconLinkBreak32 />
+        </IconButton>
+      </div>
     </div>
   );
 }
